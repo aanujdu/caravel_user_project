2bec0eeb6f212de1122d800e239a3e2f9a21d6e8
Anuj Dubey <aanujdu@gost.ece.ncsu.edu>
2022-03-20 12:28:46 -0400

    worked out a good for the config file

--- default_config.tcl
+++ default_config.tcl
@@ -1,4 +1,10 @@
 
+############################################################
+# A good configuration to start with based on documentation
+# and some perusing configs of some available designs 
+# References: user_proj_example, picorv32a, Matt Venn's repo
+############################################################
+
 # Not sure is the following two options are compulsory
 # Just copied them from the user_proj_example to be safe
 set ::env(PDK) "sky130A"
@@ -27,19 +33,19 @@ set ::env(RUN_KLAYOUT_DRC) 0
 # Since hardening a macro, 
 # Design is not core, no core ring, cant use met5
 # Compulsory options for a macro
-set ::env(DESIGN_IS_CORE) 0
-set ::env(FP_PDN_CORE_RING) 0
-set ::env(RT_MAX_LAYER) "met4"
+#set ::env(DESIGN_IS_CORE) 0
+#set ::env(FP_PDN_CORE_RING) 0
+#set ::env(RT_MAX_LAYER) "met4"
 ######################################################
 
 #### The rest of the options are mostly determined by trial and error ####
 
 # Since picorv32a is already available in OpenLane repository
 # I have simply copied the config options from there
-set ::env(SYNTH_MAX_FANOUT) 6
-set ::env(FP_CORE_UTIL) 35
-set ::env(PL_TARGET_DENSITY) [ expr ($::env(FP_CORE_UTIL)+5) / 100.0 ]
-set ::env(GLB_RESIZER_TIMING_OPTIMIZATIONS) {1}
+#set ::env(SYNTH_MAX_FANOUT) 6
+#set ::env(FP_CORE_UTIL) 35
+#set ::env(PL_TARGET_DENSITY) [ expr ($::env(FP_CORE_UTIL)+5) / 100.0 ]
+#set ::env(GLB_RESIZER_TIMING_OPTIMIZATIONS) {1}
 # Some justification on the FP flags. picorv32a is not really a tiny design
 #https://github.com/The-OpenROAD-Project/OpenLane/blob/master/docs/source/hardening_macros.md#global-placement
 # Above URL states "if your design is not a tiny design, 
@@ -50,8 +56,8 @@ set ::env(GLB_RESIZER_TIMING_OPTIMIZATIONS) {1}
 # in the RTL under USE_POWER_PINS define
 # Helps in GLSIMs as well I guess
 # And of course in routing the power rails
-set ::env(VDD_NETS) [list {vccd1}]
-set ::env(GND_NETS) [list {vssd1}]
+#set ::env(VDD_NETS) [list {vccd1}]
+#set ::env(GND_NETS) [list {vssd1}]
 ######################################################
 
 
